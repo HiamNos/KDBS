add2368a3cf75e8825522c7cb59fccec4c42d271
HiamNos <ttinh2852@gmail.com>
2025-07-12 15:33:03 +0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true
+    host: true,
+    proxy: {
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
   }
 })
